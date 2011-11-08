22a899f823ec35264cd353c34f40046ff76053cc
Alex Hart <alex.hart658@gmail.com>
2011-11-07 22:34:19 -0400

    Fix up row items for proper formatting

--- src/org/exallium/gitissues/adapters/IssueAdapter.java
+++ src/org/exallium/gitissues/adapters/IssueAdapter.java
@@ -6,6 +6,7 @@ import org.eclipse.egit.github.core.Issue;
 import org.exallium.gitissues.R;
 
 import android.content.Context;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,7 +36,11 @@ public class IssueAdapter extends ArrayAdapter<Issue> {
 		Issue i = issues.get(position);
 		if(i != null) {
 			TextView issue_no = (TextView) v.findViewById(R.id.issue_no);
-			issue_no.setText(i.getTitle());
+			TextView issue_date = (TextView) v.findViewById(R.id.issue_date);
+			TextView issue_title = (TextView) v.findViewById(R.id.issue_title);
+			issue_no.setText("#" + i.getNumber());
+			issue_title.setText(i.getTitle());
+			issue_date.setText(DateFormat.format("MM/dd/yyyy", i.getCreatedAt()));
 		}
 		
 		return v;
