569161db392de2bb433f53da04b4e5a2fb83c351
Alex Hart <alex.hart658@gmail.com>
2011-11-23 16:05:19 -0400

    Add base activity for single issue

---
+++ src/org/exallium/gitissues/SingleIssueActivity.java
@@ -0,0 +1,27 @@
+package org.exallium.gitissues;
+
+import com.sturtz.viewpagerheader.ViewPagerHeaderListener;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SingleIssueActivity extends Activity implements ViewPagerHeaderListener{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	public void onPageSelected(int position) {
+		
+	}
+
+	public void prev() {
+		
+	}
+
+	public void next() {
+		
+	}
+
+}
